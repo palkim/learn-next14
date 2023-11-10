a1d61f7add86eee05d9f677af2e4816c4b8852ba
petek.akcam <petek.akcam@kuartis.com>
2023-11-10 14:55:14 +0300

    add form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,49 +7,89 @@ import { redirect } from 'next/navigation';
 
 const InvoiceSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce.number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.'
+    }),
     date: z.string()
 });
 
 const CreateInvoice = InvoiceSchema.omit({id: true, date: true}) //these will be put automatically
 const UpdateInvoice = InvoiceSchema.omit({id: true, date: true}) 
 
-export async function createInvoice(formData: FormData) {
-    console.log("amount: ", formData.get('amount'));
-    const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    // Validate form fields using Zod
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status')
     });
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
+
     // It's usually good practice to store monetary values in cents in your database to 
     // eliminate JavaScript floating-point errors and ensure greater accuracy.
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
+    // Insert data into the database
     try {
         await sql`
             INSERT INTO invoices (customer_id, amount, status, date)
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch (error) {
+        // If a database error occurs, return a more specific error.
         return {
             message: 'Database Error: Failed to Create Invoice'
         };
     }
 
-    revalidatePath('/dashboard/invoices');
+    // Revalidate the cache for invoices and redirect the user
+    revalidatePath('/dashboard/invoices');  
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData){
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+    id: string, 
+    prevState: State,
+    formData: FormData){
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status')
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.'
+        }
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
@@ -64,11 +104,11 @@ export async function updateInvoice(id: string, formData: FormData){
         };
     }
 
-    revalidatePath('/dashboard/invoices')
+    revalidatePath('/dashboard/invoices');
     // Note how redirect is being called outside of the try/catch block. This is because redirect works by throwing an error, 
     // which would be caught by the catch block. To avoid this, you can call redirect after try/catch. 
     // redirect would only be reachable if try is successful.
-    redirect('/dashboard/invoices')
+    redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string){
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,6 +10,7 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
 
 export default function EditInvoiceForm({
   invoice,
@@ -20,9 +21,12 @@ export default function EditInvoiceForm({
 }) {
   /**This could be useful if you want to ensure that certain values, such as an id, are always included in your server actions. */
   /** By using updateInvoice WithId, we are calling the method without explicitly passing the id */
+  const initialState = { message: null, errors: {}}
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
+  console.log("state: ", state);
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -47,6 +51,16 @@ export default function EditInvoiceForm({
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+          {state.errors?.customerId ? (
+            <div
+              id="customer-error"
+              className='mt-2 text-sm text-red-500'
+            > 
+              {state.errors.customerId.map((error: string) => (
+                <p key={error}>{error}</p>
+              ))}
+            </div>
+          ) : null}
         </div>
 
         {/* Invoice Amount */}
@@ -67,6 +81,16 @@ export default function EditInvoiceForm({
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          {state.errors?.amount ? (
+          <div
+            id="customer-error"
+            className='mt-2 text-sm text-red-500'
+          > 
+            {state.errors.amount.map((error: string) => (
+              <p key={error}>{error}</p>
+            ))}
+          </div>
+          ) : null}
         </div>
 
         {/* Invoice Status */}
@@ -110,7 +134,25 @@ export default function EditInvoiceForm({
               </div>
             </div>
           </div>
+          {state.errors?.status ? (
+          <div
+            id="customer-error"
+            className='mt-2 text-sm text-red-500'
+          > 
+            {state.errors.status.map((error: string) => (
+              <p key={error}>{error}</p>
+            ))}
+          </div>
+          ) : null}
         </fieldset>
+        {state.message ? (
+          <div
+            id="customer-error"
+            className='mt-2 text-sm text-red-500'
+          > 
+            <p key={state.message}>{state.message}</p>
+          </div>
+        ) : null}
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
